2f0579cb8db46f166e31537ee2cfa34ce503be1d
Basile Vernouillet <basile64.v@gmail.com>
2021-04-08 09:20:50 +0200

    update husky version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+   purge: [
+     './src/**/*.html',
+     './src/**/*.js',
+     './src/**/*.jsx',
+   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-};
+}
