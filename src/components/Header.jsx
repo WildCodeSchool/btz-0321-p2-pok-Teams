f28904e4d0691c6407a0484212f0d217e9fe8e5f
viccabezos <cabezosvic@gmail.com>
2021-04-14 23:42:13 +0200

    changement Footer + layout (couleurs, format et autres) resultat OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="header bg-red-300 grid grid-cols-header">
-      <div className="logo bg-yellow-400 flex items-center">
+    <div className="header  grid grid-cols-header">
+      <div className="logo  flex items-center">
         <img src="src/logo.png" alt="logo"></img>
       </div>
 
@@ -20,7 +20,7 @@ export default function Header() {
         <button className="bg-yellow-400 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden sm:block">Dark mode</button>
 
         <div className="flex items-center sm:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white bg-gray-300 bg-opacity-50">
             <svg className="h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,59 +2,50 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="footer grid-cols-3 bg-green-700 flex  justify-between">
-      <div className="pokeball  ml-8 mt-4">
-        <img className="object-contain h-8" src="src/pokeball.png" alt="alt"></img>
-        <img className="object-contain h-8" src="src/copyright.png" alt="alt"></img>
+    <div className="footer bg-gray-300 bg-opacity-50 grid grid-flow-col auto-cols-auto flex mx-2 mt-1.5 mb-1.5 justify-center rounded-lg  border border-blue-200 ">
+      <div className="pokeball mt-4 ml-4 mr-4 ">
+        <img className="" src="src/pokeball.png" alt="alt"></img>
+        <img className="mt-14" src="src/copyright.png" alt="alt"></img>
       </div>
 
-      <div className=" listing  flex flex-col  items-center ">
-        <ul>
-          <li className="mx-6">The pokemon company</li>
-          <li className="mx-6">
-            <p>Press</p>
-          </li>
-          <li className="mx-6">
-            <p>Customer service</p>
-          </li>
-          <li className="mx-6">
-            <p>Legal terms</p>
-          </li>
-          <li className="mx-6">
-            <p>News</p>
-          </li>
-          <li className="mx-6">
-            <p>ToS</p>
-          </li>
+      <div className="infos grid grid-flow-col m-2">
+        <ul className="list-none">
+          <li className=" font-extrabold">The pokemon company</li>
+          <li className="">Press</li>
+          <li className="">Customer service</li>
+          <li className="">Legal terms</li>
+          <li className="">News</li>
+          <li className="">ToS</li>
         </ul>
       </div>
-      <div>
-        <span>Poke Team</span>
-
-        <p className="mx-6">Joseph</p>
-        <p className="mx-6">Victoria</p>
-        <p className="mx-6">Benjamin</p>
+      <div className="infos grid grid-flow-col m-2">
+        <ul className="list-none">
+          <li className=" font-extrabold">Poke Team</li>
+          <li className="">Joseph</li>
+          <li className="">Victoria</li>
+          <li className="">Banjamin</li>
+        </ul>
       </div>
 
-      <div className="reseaux flex flex-col place-items-end mr-8 mt-6">
+      <div className="reseaux grid grid-flow-col m-4 space-y-6  ">
         <ul>
           <li>
-            <img className="object-contain h-8" src="src/facebook.png" alt="alt"></img>
+            <img className="" src="src/facebook.png" alt="alt"></img>
           </li>
           <li>
-            <img className="object-contain h-8" src="src/github.png" alt="alt"></img>
+            <img className="" src="src/github.png" alt="alt"></img>
           </li>
           <li>
-            <img className="object-contain h-8" src="src/insta.png" alt="alt"></img>
+            <img className=" " src="src/insta.png" alt="alt"></img>
           </li>
           <li>
-            <img className="object-contain h-8" src="src/linkedin.png" alt="alt"></img>
+            <img className=" " src="src/linkedin.png" alt="alt"></img>
           </li>
           <li>
-            <img className="object-contain h-8" src="src/twitter.png" alt="alt"></img>
+            <img className=" " src="src/twitter.png" alt="alt"></img>
           </li>
           <li>
-            <img className="object-contain h-8" src="src/youtube.png" alt="alt"></img>
+            <img className=" " src="src/youtube.png" alt="alt"></img>
           </li>
         </ul>
       </div>
