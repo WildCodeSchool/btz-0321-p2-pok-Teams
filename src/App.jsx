9b7c527f3174ddaedf3de7f3a1e29c874492c989
viccabezos <cabezosvic@gmail.com>
2021-04-15 16:44:16 +0200

    changements WireFrame Footer pas fini

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="grid grid-cols-1 grid-rows-strp w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500">
+    <div className="grid  grid-rows-PhoneRows w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500 pc:grid-rows-PCRows">
       <Header />
       <div className="bg-gray-400 ">Main</div>
       <Footer />
