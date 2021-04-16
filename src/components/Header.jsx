45d17c5d1adc771f22b9342e679b531067e06f32
viccabezos <cabezosvic@gmail.com>
2021-04-16 13:43:04 +0200

    actu config PC mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,26 @@ export default function Header() {
   return (
     <div className="grid grid-cols-PhoneHeaderCol">
       <div className="logo  flex items-center ml-2">
-        <img className="h-14 pc:h-16" src="src/logo.png" alt="logo"></img>
+        <img className="  h-14 pc:h-16" src="src/logo.png" alt="logo"></img>
       </div>
 
-      <div className=" inline-flex justify-end mx-8 flex-wrap text-lg lg:w-auto it">
-        <div className=" hidden sm:flex mx-8 text-center items-center ">
-          <span>how to play</span>
-
-          <span>my team</span>
-
-          <span>pokedex</span>
+      <div className="MENU inline-flex justify-end mx-8 flex-nowrap text-lg ">
+        <div className=" hidden pc:flex mx-8 text-center items-center font-Quantico text-2xl space-x-10">
+          <ul>
+            <li>how to play</li>
+          </ul>
+          <ul>
+            <li>my team</li>
+          </ul>
+          <ul>
+            <li>pokedex</li>
+          </ul>
         </div>
-        <button className="bg-blue-900 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden sm:block">music</button>
-        <button className="bg-indigo-600 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden sm:block">Login</button>
-        <button className="bg-yellow-400 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden sm:block">Dark mode</button>
+        <button className="bg-gray-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden pc:block mr-8 mt-2">music</button>
+        <button className="bg-gray-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden pc:block mr-8 mt-2">Login</button>
+        <button className="bg-gray-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded hidden pc:block mt-2">Dark mode</button>
 
-        <div className="flex items-center sm:hidden">
+        <div className="flex items-center pc:hidden">
           <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white bg-gray-300 bg-opacity-50">
             <svg className="h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
