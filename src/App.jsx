209b9b934e7f82480ab44aff5dbaf69ad49f6025
viccabezos <cabezosvic@gmail.com>
2021-04-21 12:09:24 +0200

    changement structure code Footer avec Basile, boite grise recuperee

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/footer';
+import DisplayEmployee from './components/ApiCall';
+import axios from 'axios';
 
 function App() {
+  const [employee, setEmployee] = useState(null);
+
+  const getEmployee = () => {
+    axios
+      .get('https://randomuser.me/api?nat=en')
+      .then((response) => response.data)
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
   return (
     <div className="grid  grid-rows-PhoneRows w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500 pc:grid-rows-PCRows">
       <Header />
-      <div className="bg-gray-400 mt-6 ">Main</div>
+      <div className="bg-gray-400 mt-6 ">
+        Main
+        <button type="button" className="bg-yellow-500" onClick={getEmployee}>
+          Get employee
+        </button>
+        <DisplayEmployee employee={employee} />
+      </div>
       <Footer />
     </div>
   );
