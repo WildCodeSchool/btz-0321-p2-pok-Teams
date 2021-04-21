1bb727ca9a99344feb4d55730e754b647574e205
Benjaminlavigneducadet <benjamin.lavigneducadet@hotmail.fr>
2021-04-21 14:28:59 +0200

    code Body clean

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/footer';
-
+import Body from './components/Body';
 function App() {
   return (
     <div className="grid  grid-rows-PhoneRows w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500 pc:grid-rows-PCRows">
       <Header />
-      <div className="bg-gray-400 ">Main</div>
+      <div className="bg-gray-400 ">Main
+      <Body />
+      
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/Body.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Body() {
+  return (
+
+            <div className="infos  text-center font-Quantico h-64 grid grid-rows-3 grid-flow-col gap-4 rounded-md">   
+              <div className="bg- bg-red-600 h-64 grid grid-rows-3 grid-flow-col gap-4 rounded-md text-justify ..." > 
+        
+                <p>Welcome Trainer, 
+                    You are about to start a great adventure, but your path is full of pitfalls !
+                    Luckily, your pokémon friends are here to help.
+
+                    Select a destination on your map and battle trainers to clear your way through glory !
+                    Victories might also reward you with new friends and more...
+
+                    Your pokémons have many abilities, different types and strengths/weaknesses.
+                    It is up to you to figure out the best team configuration to win each battle.
+                </p>
+              </div>
+            </div>
+  );
+}
