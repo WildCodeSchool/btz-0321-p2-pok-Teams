fa701641cd45ae069bd9ed0d2ef8a298bf14d2e1
Benjaminlavigneducadet <benjamin.lavigneducadet@hotmail.fr>
2021-04-22 18:30:00 +0200

    ajout image plus text

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/footer';
-import Body from './components/Body';
+import Howtoplay from './components/Howtoplay';
 function App() {
   return (
     <div className="grid  grid-rows-PhoneRows w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500 pc:grid-rows-PCRows">
       <Header />
-<<<<<<< HEAD
-      <div className="bg-gray-400 ">Main
-      <Body />
+      <div className="bg-gray-400 ">
+      <Howtoplay />
       
       </div>
-=======
-      <div className="bg-gray-400 mt-6 ">Main</div>
->>>>>>> 3a6da9883861bd90276924e8d265f37ee051c7e1
       <Footer />
     </div>
   );
--- src/components/Howtoplay.jsx
+++ src/components/Howtoplay.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Body() {
+export default function Howtoplay() {
   return (
 
-            <div className="infos  text-center font-Quantico h-64 grid grid-rows-3 grid-flow-col gap-4 rounded-md">   
-              <div className="bg- bg-red-600 h-64 grid grid-rows-3 grid-flow-col gap-4 rounded-md text-justify ..." > 
-        
-                <p>Welcome Trainer, 
+    <div className="infos  text-center font-Quantico w-screen">
+          <div className="grid grid-cols-1 rows-3 text-justify ...  bg- bg-red-600 h-64">
+            
+                  <p>Welcome Trainer, 
                     You are about to start a great adventure, but your path is full of pitfalls !
                     Luckily, your pokémon friends are here to help.
 
@@ -15,8 +15,12 @@ export default function Body() {
 
                     Your pokémons have many abilities, different types and strengths/weaknesses.
                     It is up to you to figure out the best team configuration to win each battle.
-                </p>
-              </div>
-            </div>
+                  </p>
+                  </div>
+    <div>
+          <img className= "mt-24 pc:mt-0 pc:h-8 pc:ml-10"  src="src/Léviator-RFVF.png" alt=""/></div>
+
+          <p>Catch Em All and make them Battle</p>
+    </div> 
   );
 }
