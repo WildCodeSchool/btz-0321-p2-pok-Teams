558bdb547886836814a25b7541bed3adf941f974
Benjaminlavigneducadet <benjamin.lavigneducadet@hotmail.fr>
2021-04-23 14:35:37 +0200

    mise en page ajout images

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ function App() {
   return (
     <div className="grid  grid-rows-PhoneRows w-screen min-h-screen  bg-gradient-to-r from-yellow-300 to-yellow-500 pc:grid-rows-PCRows">
       <Header />
-      <div className="bg-gray-400 ">
+      
       <Howtoplay />
       
-      </div>
+      
       <Footer />
     </div>
   );
