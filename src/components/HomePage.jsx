e7ee843cfe218dc90fa50fceb0a83c0c0c8dc978
Tweaka <vzxpk@yahoo.fr>
2021-04-28 09:48:53 +0200

    modif homepage2

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
       <img className=" absolute bg-cover bg-no-repeat z-40 h-full w-full  " src={HomeBg} alt="wallpaper" />
       <div className=" flex justify-center bottom-14 py-52 pc:py-20">
         <img className="absolute z-50 w-10/12 pc:w-1/2 " src={Logo} alt="logo" />
-        <button className="absolute z-50 top-1/2  bg-red-700 hover:bg-red-500 font-extrabold py-2 px-12 my-40 pc:w-1/4 pc:p-3 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md .shadow-inner ">
+        <button className="absolute z-50 top-1/2  bg-red-700 hover:bg-red-500 font-extrabold py-2 px-12 my-40 pc:w-1/4 pc:p-3 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md  ">
           <Link to="register">PLAY</Link>
         </button>
       </div>
