2c1df06d5d4f9a78b8180bbae84e1c0e32ff2488
Tweaka <vzxpk@yahoo.fr>
2021-04-30 14:42:16 +0200

    mise en page avant prez

--- src/components/MyCollection.jsx
+++ src/components/MyCollection.jsx
@@ -12,18 +12,18 @@ export default function MyCollection({ url, name }) {
   return (
     <div className="flex flex-col align-center justify-center font-Quantico ">
       <div className="">
-        <div className="bg-gray-50 w-40 border-2 border-blue-300 rounded-lg flex flex-col text-g text-center m-1.5 shadow-2xl mx-3 my-2 ">
-          <div className="flex justify-around bg-blue-200">
+        <div className="bg-gray-50 w-40 border-2 border-teal-300 rounded-lg flex flex-col text-g text-center m-1.5 shadow-2xl mx-3 my-2 ">
+          <div className="flex justify-around bg-teal-500 rounded-t-lg ">
             <h2 className="text-lg">{data.name}</h2>
             <p className=" text-lg  "> {data.id}</p>
           </div>
           <img className="flex justify-center " src={data.sprites.front_default} alt={data.name} />
-          <div className="flex justify-around bg-blue-200 ">
+          <div className="flex justify-around bg-teal-500 rounded-b-lg">
             <p>{data.types[0].type.name}</p>
             <p className="text-yellow-500">{data.base_experience} xp </p>
           </div>
         </div>
-        <button className="bg-blue-600 border-2 border-black rounded w-10/12 shadow-2xl ml-4">add to team</button>
+        <button className="bg-teal-700 border-2 border-black rounded w-10/12 shadow-2xl ml-4">add to team</button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
     },
 
     extend: {
+      colors: {
+        teal: {
+          50: '#edfafa',
+          100: '#d5f5f6',
+          200: '#aae7ea',
+          300: '#69c8ce',
+          400: '#13a2ad',
+          500: '#0d828d',
+          600: '#056874',
+          700: '#06505c',
+          800: '#043744',
+          900: '#01222f',
+        },
+      },
+
       gridTemplateRows: {
         PhoneRows: '0.1fr 1fr 0.8fr',
         PCRows: '',
