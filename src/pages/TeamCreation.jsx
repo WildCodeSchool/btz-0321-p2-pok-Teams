9a9858eded4bca68475034a3b73199bfc67fe354
viccabezos <cabezosvic@gmail.com>
2021-05-07 14:32:02 +0200

    last cleaning

--- src/pages/TeamCreation.jsx
+++ src/pages/TeamCreation.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PokemonList from '../components/MyTeam';
 import Red from '../components/img/redt.png';
-import { XCircleIcon } from '@heroicons/react/solid';
 
 export default function TeamCreation() {
   const [team, setTeam] = useState([]);
@@ -13,17 +12,13 @@ export default function TeamCreation() {
       <div className="flex justify-between xs:flex-wrap  py-6 px-4">
         <div className="flex ">
           <img className="w-6 xs:w-10 xs:h-10" src={Red} alt="playersTrainer" />
-          <h2 className="text-4xl xs:text-xl font-extrabold">&#60;Player&#62;&#39;s Team</h2>
+          <h2 className="text-4xl xs:text-xl font-extrabold">Joseph&#39;s Team</h2>
         </div>
         <p className="text-4xl xs:text-xl xs:pl-10">current team {team.length}/6</p>
       </div>
       <div className="flex xs:flex-wrap ">
         {team.map((teammate) => (
           <div key={teammate.data}>
-            {/* <button>
-              <XCircleIcon className="w-4 text-teal-600" />{' '}
-            </button> */}
-
             <div className="bg-gray-50 text-gray-200 w-40 border-2 border-teal-300 rounded-lg flex flex-col text-g text-center m-1.5 shadow-2xl mx-2 my-2 ">
               <div className="flex justify-around bg-teal-500 rounded-t-lg ">
                 <h2 className="text-lg">{teammate.name}</h2>
